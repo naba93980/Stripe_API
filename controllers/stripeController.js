f4a466d0f7189a2b7be7e02d246c4f918effad66
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-11-05 23:49:28 +0530

    stripeController tp create paymentIntent and send clientSecret as response back to the app

--- controllers/stripeController.js
+++ controllers/stripeController.js
@@ -2,7 +2,15 @@ const stripe = require('stripe')(process.env.SECRET_KEY);
 
 const stripeController = async (req, res) => {
     const { purchase, total_amount, shipping_fee } = req.body;
-    res.send('stripe route');
+    const calculateOrderAmount = () => {
+        return total_amount + shipping_fee;
+    }
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: calculateOrderAmount(),
+        currency: 'inr'
+    });
+    console.log(paymentIntent);
+    res.json({ clientSecret: paymentIntent.client_secret });
 }
 
 module.exports = stripeController;
