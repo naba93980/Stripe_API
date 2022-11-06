a146737bf5b9356999c7339d4f597de422ab82e4
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-11-06 23:55:37 +0530

    checked with new id

--- cancel.js
+++ cancel.js
@@ -4,7 +4,7 @@ const stripe = require('stripe')(process.env.SECRET_KEY);
 async function cancelIntent() {
     try {
         const paymentIntent = await stripe.paymentIntents.cancel(
-            'pi_3M0X4SSBPxz6nNvI1f1ZWvmK'
+            'pi_3M18S0SBPxz6nNvI00bkyPbi'
         );
         console.log(paymentIntent);
     } catch (error) {
