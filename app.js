fc8d0a0f7bef41259684e7ebf38fce8931e4a3e0
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-11-06 23:55:05 +0530

    added /webhook route

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 require('dotenv').config();
 require('express-async-errors');
 const stripeController = require('./controllers/stripeController');
+const webhook = require('./controllers/webhook');
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
-
-
 
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
-app.use(express.json());
 app.use(express.static('./public'));
 
 // controller
-app.post('/stripe',stripeController);
+app.post('/stripe', express.json() ,stripeController);
+app.post('/webhook', bodyParser.raw({ type: 'application/json' }), webhook);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
